c48f77d91d106261a8de7fb527d91a318c2b1ff0
Ibis Sunrosa <drsunrosa@gmail.com>
2023-11-12 18:26:36 +0000

    Prompt items as Vec of tuples

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     let mut rl = rustyline::DefaultEditor::new().unwrap();
-    println!("{}", prompt_float(&mut rl));
+    let items = prompt_items(&mut rl);
+    println!("{:?}", items);
 }
 
-fn prompt(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>) -> String {
-    let readline = rl.readline("> ");
+fn prompt(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>, p: &str) -> String {
+    let readline = rl.readline(p);
     match readline {
         Ok(line) => line,
         Err(_) => {
@@ -14,10 +15,10 @@ fn prompt(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>) -> St
     }
 }
 
-fn prompt_float(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>) -> f32 {
+fn prompt_i32(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>, p: &str) -> i32 {
     loop {
-        let input_string = prompt(rl);
-        match input_string.parse::<f32>() {
+        let input_string = prompt(rl, p);
+        match input_string.parse::<i32>() {
             Ok(t) => {
                 break t;
             }
@@ -27,4 +28,18 @@ fn prompt_float(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>)
             }
         }
     }
+}
+
+fn prompt_items(
+    rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>,
+) -> Vec<(String, i32, i32)> {
+    let item_count = prompt_i32(rl, "Item count > ");
+    let mut items: Vec<(String, i32, i32)> = Vec::new();
+    for n in 1..(item_count + 1) {
+        let item_name = prompt(rl, format!("Item {} name > ", n).as_str());
+        let item_amount = prompt_i32(rl, format!("Item {} amount > ", n).as_str());
+        let item_stack_size = prompt_i32(rl, format!("Item {} stack size > ", n).as_str());
+        items.push((item_name, item_amount, item_stack_size));
+    }
+    items
 }
