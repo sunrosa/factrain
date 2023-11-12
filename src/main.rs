103911b8ac47e7e0792fee46127a3a90a5ed7418
Ibis Sunrosa <drsunrosa@gmail.com>
2023-11-12 18:53:38 +0000

    Documentation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 fn main() {
     let mut rl = rustyline::DefaultEditor::new().unwrap();
     let items = prompt_items(&mut rl);
-    println!("{:?}", items);
+
+    // Debug contents of items Vec in debug mode.
+    #[cfg(debug_assertions)]
+    {
+        println!("{:?}", items);
+    }
 }
 
+/// Prompt through STDIN for user string input using rustyline `rl` with prompt `p`.
+///
+/// # Arguments
+/// * `rl` - Rustyline [Editor](rustyline::Editor) used to read user's input.
+/// * `p` - User prompt given to the user to signal the need for input.
 fn prompt(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>, p: &str) -> String {
     let readline = rl.readline(p);
     match readline {
         Ok(line) => line,
-        Err(_) => {
-            println!("No input");
+        Err(..) => {
+            println!("Input error.");
             "".to_owned()
         }
     }
 }
 
+/// Prompt through STDIN for user integer input using rustyline `rl` with prompt `p`. This function will repeat the prompt until a valid integer is provided.
+///
+/// # Arguments
+/// * `rl` - Rustyline [Editor](rustyline::Editor) used to read user's input.
+/// * `p` - User prompt given to the user to signal the need for input.
 fn prompt_i32(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>, p: &str) -> i32 {
     loop {
         let input_string = prompt(rl, p);
@@ -30,6 +45,16 @@ fn prompt_i32(rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>, p
     }
 }
 
+/// Prompt through STDIN for user integer input using rustyline `rl` with prompt `p`. This function will ask the user for the number of items they wish to list. Then, a looping prompt will ask them for the item name, item amount, and item stack size.
+///
+/// # Arguments
+/// * `rl` - Rustyline [Editor](rustyline::Editor) used to read user's input.
+///
+/// # Returns
+/// A [Vec] containing tuples containing:
+/// * __0__: The item's name. It does not have to match the item's name inside of Factorio. It is purely for reference.
+/// * __1__: The amount of the item used on the factory floor used in an arbitrary amount of time. This amount of time must be equal for all items referenced.
+/// * __2__: The item's stack size.
 fn prompt_items(
     rl: &mut rustyline::Editor<(), rustyline::history::FileHistory>,
 ) -> Vec<(String, i32, i32)> {
